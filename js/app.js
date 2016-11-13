4fa9c9f05e4ec6b04831f09fd197dff14a3be837
aneylon <arlen.m.neylon@gmail.com>
2016-11-12 22:26:08 -0800

    (feat) minor bug fixes and begin number parsing

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 var LearnToCount = (function(){
   return {
     currentNumber: 0,
+    currentNumberText: '',
+    currentLang: '',
     selected: function(value){
-      if ( value === '日本語')
-        console.log(japanese)
-      else
-        console.log(value)
+      if(value)
+      if(value = 'korean')
+        currentLang = korean
+      if(value = 'japanese')
+        currentLang = japanese
+      if(value = 'chinese')
+        currentLang = chinese
 
       this.nextNumber();
-    },
-    pickedLang: function(){
-      console.log('picked')
     },
     pickRandom: function(max, min){
       max = max || 2
@@ -32,6 +34,14 @@ var LearnToCount = (function(){
     setText: function(elementID, text){
       document.getElementById(elementID).innerHTML = text
     },
+    showPronounciation: function(){
+      var showCheck = document.getElementById('showPronounciation').checked
+      if(showCheck){
+        this.addClassById('pronunciation', 'visible')
+      } else {
+        this.addClassById('pronunciation', 'invisible')
+      }
+    },
     addClassById: function(elementID, newClass){
       document.getElementById(elementID).className = newClass
     },
@@ -42,11 +52,30 @@ var LearnToCount = (function(){
         length = 1
         lengthField.value = ''
       }
+      if (length > 16) {
+        length = 16
+        lengthField.value = 16
+      }
       return length
     },
     nextNumber: function(){
       this.currentNumber = this.makeNumber(this.getLength())
-      this.setText('number',this.currentNumber)
+      this.currentNumberText = this.numberToText(this.currentNumber)
+      this.setText('number',this.currentNumberText)
+    },
+    numberToText: function(curNum){
+      curNum = Number(curNum)
+      var txtNum = ''
+      var parts = []
+      if(curNum === 0){
+        parts.push(curNum)
+      }
+      while(curNum > 0){
+        parts.push(curNum % 10000)
+        curNum = Math.floor(curNum / 10000)
+      }
+      return parts.reverse()
+      return txtNum
     },
     checkNumber: function(event){
       if(event.which === 13) {
@@ -55,8 +84,10 @@ var LearnToCount = (function(){
         answerField.value = ''
         if(answer === this.currentNumber){
           this.addClassById('checked', 'correct')
+          document.getElementById('checked').innerHTML = 'Correct'
         } else {
           this.addClassById('checked', 'incorrect')
+          document.getElementById('checked').innerHTML = 'Incorrect'
         }
         this.nextNumber()
       }
