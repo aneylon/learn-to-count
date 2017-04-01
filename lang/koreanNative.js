33f3d2397ef66eff4d129588f6fbc59ff2dabc8c
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-04-01 16:08:59 -0700

    add native korean numbers

---
+++ lang/koreanNative.js
@@ -0,0 +1,8 @@
+var koreanNative = {
+  zero:['영','yeong'],
+  one:[['',''],['하나','hana'],['둘','dul'],['셋','set'],['넷','net'],['다섯','daseot'],['여섯','yeoseot'],['일곱','ilgop'],['여덟','yeodeol'],['아홉','ahop']],
+  ten:[['',''],['열','yeol'],['스물','seumul'],['서른','seoreun'],['마흔','maheun'],['쉰','swin'],['예순','yesun'],['일흔','ilheun'],['여든','yeodeun'],['아흔','aheun']],
+  hundred:[['',''],['온','on'],['둘온','dulon'],['셋온','seton'],['넷온','neton'],['다섯온','daseoton'],['여섯온','yeoseoton'],['일곱온','ilgopon'],['여덟온','yeodeolon'],['아홉온','ahopon']],
+  thousand:[['',''],['즈믄','jeumeun'],['둘즈믄','duljeumeun'],['셋즈믄','setjeumeun'],['넷즈믄','netjeumeun'],['다섯즈믄','daseotjeumeun'],['여섯즈믄','yeoseotjeumeun'],['일곱즈믄','ilgopjeumeun'],['여덟즈믄','yeodeoljeumeun'],['아홉즈믄','ahopjeumeun']],
+  suffix:[['',''],['드먼/골','deumeon/gol'],['잘','jal'],['울','ul']]
+}
